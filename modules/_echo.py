c20406cff20ac1a18dcf757cea2d0418041d57fe
HanTools233 <hantools@foxmail.com>
2022-12-23 15:54:29 +0800

    新增：涩图组件（你被骗了）

--- modules/_echo.py
+++ modules/_echo.py
@@ -1,8 +1,4 @@
-#  Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
-#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
-#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
-#  Vestibulum commodo. Ut rhoncus gravida arcu.
+#  本项目遵守 AGPL-3.0 协议，项目地址：https://github.com/daizihan233/MiraiHanBot
 
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
---
+++ modules/setu.py
@@ -0,0 +1,32 @@
+#  本项目遵守 AGPL-3.0 协议，项目地址：https://github.com/daizihan233/MiraiHanBot
+
+import time
+
+from graia.ariadne.app import Ariadne
+from graia.ariadne.event.message import GroupMessage
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import Image
+from graia.ariadne.message.parser.base import MatchContent
+from graia.ariadne.model import Group
+from graia.saya import Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+
+channel = Channel.current()
+channel.name("来份涩图")
+channel.description("人类有三大欲望……")
+channel.author("HanTools")
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage],
+        decorators=[MatchContent("涩图来")],
+    )
+)
+async def test(app: Ariadne, group: Group):
+    async with Ariadne.service.client_session as session:
+        async with session.get("https://i1.hdslb.com/bfs/archive/5242750857121e05146d5d5b13a47a2a6dd36e98.jpg") as r:
+            data = await r.read()
+    b_msg = await app.send_group_message(group, MessageChain(Image(data_bytes=data)))
+    time.sleep(120)
+    await app.recall_message(b_msg)
