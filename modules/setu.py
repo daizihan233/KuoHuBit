51b645a0c73bba313c642742d24d31cefd72e716
HanTools233 <hantools@foxmail.com>
2022-12-23 16:32:18 +0800

    修复&新增：修复涩图在一个小时内显示一样的BUG，新增了一句提示语

--- modules/setu.py
+++ modules/setu.py
@@ -1,12 +1,15 @@
 #  本项目遵守 AGPL-3.0 协议，项目地址：https://github.com/daizihan233/MiraiHanBot
 
 #  本项目遵守 AGPL-3.0 协议，项目地址：https://github.com/daizihan233/MiraiHanBot
+
+#  本项目遵守 AGPL-3.0 协议，项目地址：https://github.com/daizihan233/MiraiHanBot
 import asyncio
 
+import requests
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.message.element import Image
+from graia.ariadne.message.element import Image, At, Plain
 from graia.ariadne.message.parser.base import MatchContent
 from graia.ariadne.model import Group
 from graia.saya import Channel
@@ -30,8 +33,13 @@ channel.author("HanTools")
         decorators=[MatchContent("涩图来")],
     )
 )
-async def test(app: Ariadne, group: Group):
-    data = botfunc.session.get('https://i1.hdslb.com/bfs/archive/5242750857121e05146d5d5b13a47a2a6dd36e98.jpg').content
+async def test(app: Ariadne, group: Group, event: GroupMessage):
+    # 涩图不一样，这里不能使用缓存
+    data = requests.get('https://i1.hdslb.com/bfs/archive/5242750857121e05146d5d5b13a47a2a6dd36e98.jpg').content
     b_msg = await app.send_group_message(group, MessageChain(Image(data_bytes=data)))
     await asyncio.sleep(botfunc.get_config('recall'))
     await app.recall_message(b_msg)
+    await app.send_message(
+        group,
+        MessageChain([At(event.sender.id), Plain("看完了吗？我撤回了")]),
+    )
