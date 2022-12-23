996a0a611e01730809a262a32a266445f3e88cdd
HanTools233 <hantools@foxmail.com>
2022-12-23 16:54:57 +0800

    新增：摸鱼日历

--- modules/fish.py
+++ modules/fish.py
@@ -2,13 +2,12 @@
 
 #  本项目遵守 AGPL-3.0 协议，项目地址：https://github.com/daizihan233/MiraiHanBot
 
-import asyncio
+import json
 
-import requests
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.message.element import Image, At, Plain
+from graia.ariadne.message.element import At, Plain
 from graia.ariadne.message.parser.base import MatchContent
 from graia.ariadne.model import Group
 from graia.saya import Channel
@@ -17,24 +16,21 @@ from graia.saya.builtins.broadcast.schema import ListenerSchema
 import botfunc
 
 channel = Channel.current()
-channel.name("来份涩图")
-channel.description("人类有三大欲望……")
+channel.name("摸鱼日记")
+channel.description("上班是不可能上班的~")
 channel.author("HanTools")
 
 
 @channel.use(
     ListenerSchema(
         listening_events=[GroupMessage],
-        decorators=[MatchContent("涩图来")],
+        decorators=[MatchContent("鱼")],
     )
 )
-async def setu(app: Ariadne, group: Group, event: GroupMessage):
-    # 涩图不一样，这里不能使用缓存
-    data = requests.get(botfunc.get_config('setu_api')).content
-    b_msg = await app.send_group_message(group, MessageChain(Image(data_bytes=data)))
-    await asyncio.sleep(botfunc.get_config('recall'))
-    await app.recall_message(b_msg)
+async def fish(app: Ariadne, group: Group, event: GroupMessage):
+    data: str = json.loads(botfunc.session.get("http://bjb.yunwj.top/php/mo-yu/php.php").text)['wb']
+    data: str = data.replace('【换行】', '\n')
     await app.send_message(
         group,
-        MessageChain([At(event.sender.id), Plain(" 看完了吗？我撤回了")]),
+        MessageChain([At(event.sender.id), Plain(f" \n{data}")]),
     )
