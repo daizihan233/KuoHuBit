c0aaa7039de9e50458ff3f4e66d93635e3d9e36b
HanTools233 <hantools@foxmail.com>
2022-12-25 14:40:08 +0800

    去掉了提示（妈的吵死了

--- modules/setu.py
+++ modules/setu.py
@@ -1,5 +1,7 @@
 #  本项目遵守 AGPL-3.0 协议，项目地址：https://github.com/daizihan233/MiraiHanBot
 
+#  本项目遵守 AGPL-3.0 协议，项目地址：https://github.com/daizihan233/MiraiHanBot
+
 import asyncio
 import random
 
@@ -7,7 +9,7 @@ from aiohttp import ClientSession
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.message.element import Image, At, Plain
+from graia.ariadne.message.element import Image
 from graia.ariadne.message.parser.base import MatchContent
 from graia.ariadne.model import Group
 from graia.saya import Channel
@@ -43,7 +45,3 @@ async def setu(app: Ariadne, group: Group, event: GroupMessage):
     b_msg = await app.send_group_message(group, MessageChain(Image(data_bytes=data)))
     await asyncio.sleep(botfunc.get_config('recall'))
     await app.recall_message(b_msg)
-    await app.send_message(
-        group,
-        MessageChain([At(event.sender.id), Plain(" 看完了吗？我撤回了")]),
-    )
