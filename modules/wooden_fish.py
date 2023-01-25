324d824f06e25730b8a4420ed59bc0127ec68e39
HanTools233 <hantools@foxmail.com>
2023-01-25 21:09:18 +0800

    fix

--- modules/wooden_fish.py
+++ modules/wooden_fish.py
@@ -386,7 +386,16 @@ async def update_fish(app: Ariadne, group: Group, event: GroupMessage):
                             result[4] = np.log10(np.power(10, result[4]) + result[3])
                         if np.log10(result[4]) >= 1:
                             result[5] = np.log10(np.power(10, result[5]) + result[4])
-                        if np.power(10, result[5]) + result[4] >= result[2] + 2:
+                        if result[4] >= result[2] + 2:
+                            await else_sql("UPDATE woodenfish SET level = level+1, e = e-level+2 WHERE uid = %s",
+                                           (event.sender.id,))
+                            await app.send_message(
+                                group,
+                                "木鱼升级成功辣！（喜）"
+                            )
+                        elif np.power(10, result[5]) + result[4] >= result[2] + 2:
+                            await else_sql("UPDATE woodenfish SET level = level+1, e = 0, ee = ee-%s WHERE uid = %s",
+                                           (np.log10(result[2] + 2 - result[4]), event.sender.id))
                             await app.send_message(
                                 group,
                                 "木鱼升级成功辣！（喜）"
--- modules/wooden_fish_cht.py
+++ modules/wooden_fish_cht.py
@@ -293,9 +293,16 @@ async def update_fish(app: Ariadne, group: Group, event: GroupMessage):
                         (event.sender.id,)
                     )
                 else:
-                    if np.power(10, result[5]) + result[4] >= result[2] + 2:
+                    if result[4] >= result[2] + 2:
                         await else_sql("UPDATE woodenfish SET level = level+1, e = e-level+2 WHERE uid = %s",
                                        (event.sender.id,))
+                        await app.send_message(
+                            group,
+                            "木鱼升級成功辣！（喜）"
+                        )
+                    elif np.power(10, result[5]) + result[4] >= result[2] + 2:
+                        await else_sql("UPDATE woodenfish SET level = level+1, e = 0, ee = ee-%s WHERE uid = %s",
+                                       (np.log10(result[2] + 2 - result[4]), event.sender.id))
                         await app.send_message(
                             group,
                             "木鱼升級成功辣！（喜）"
