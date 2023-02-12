bd36398def899811403be63a8191cbdd47fb9390
HanTools233 <hantools@foxmail.com>
2023-02-12 17:03:20 +0800

    修复修复修复

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ cursor.execute("""create table if not exists woodenfish
     hit_count int          default 0   not null comment '一周期内的调用次数'
 ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_0900_ai_ci" """)
 cursor.execute("""CREATE TABLE IF NOT EXISTS `six` ( 
-`uid` bigint UNSIGNED NOT NULL COMMENT 'QQ号' ,
+`uid` bigint UNSIGNED NOT NULL PRIMARY KEY COMMENT 'QQ号' ,
 `count` int UNSIGNED NOT NULL DEFAULT 0 COMMENT '6 的次数',
 `ti` bigint UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后一次消息发送时间'
 ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_0900_ai_ci" """)
