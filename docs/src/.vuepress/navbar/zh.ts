f7392a81dc8dba8b32e00a0bfa2af9deef45841b
HanTools233 <hantools@foxmail.com>
2023-04-29 23:43:10 +0800

    fixbug

--- docs/src/.vuepress/navbar/zh.ts
+++
@@ -1,7 +0,0 @@
-import {navbar} from "vuepress-theme-hope";
-
-export const zhNavbar = navbar([
-    "/",
-    {text: "使用", icon: "discover", link: "/demo/"},
-    {text: "构建", icon: "copy", link: "/guide/"}
-]);
