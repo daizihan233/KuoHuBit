f4acec4a8757a5672fddfefbfef2a22244f56c17
HanTools233 <hantools@foxmail.com>
2023-06-27 14:43:35 +0800

    echo：若原消息撤回，机器人的消息也会撤回

--- modules/zh_cn/echo.py
+++ modules/zh_cn/echo.py
@@ -1,10 +1,14 @@
 from graia.ariadne.app import Ariadne
-from graia.ariadne.event.message import GroupMessage
+from graia.ariadne.event.message import GroupMessage, ActiveGroupMessage
+from graia.ariadne.event.mirai import GroupRecallEvent
+from graia.ariadne.message import Source
 from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.message.parser.base import DetectPrefix
 from graia.ariadne.model import Group
 from graia.saya import Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
+
+import botfunc
 
 channel = Channel.current()
 channel.name("echo")
@@ -13,11 +17,16 @@ channel.author("HanTools")
 
 
 @channel.use(ListenerSchema(listening_events=[GroupMessage]))
-async def echo(app: Ariadne, group: Group, message: MessageChain = DetectPrefix("/echo ")):
-    if str(message).startswith('/echo'):  # 防止注入
-        pass  # 这里不需要进行提示，防止出现机器人反复提示的问题
-    else:
-        await app.send_message(
+async def echo(app: Ariadne, group: Group, source: Source, message: MessageChain = DetectPrefix("/echo ")):
+    if not str(message).startswith('/echo'):
+        m: ActiveGroupMessage = await app.send_group_message(
             group,
             message,
         )
+        botfunc.r.hset('echo', source.id, m.source.id)
+
+
+@channel.use(ListenerSchema(listening_events=[GroupRecallEvent]))
+async def echo(app: Ariadne, group: Group, event: GroupRecallEvent):
+    if botfunc.r.hexists("echo", event.message_id):
+        await app.recall_message(botfunc.r.hget("echo", event.message_id), group)
