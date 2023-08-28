adf02494854a8f9d2744757152b63cf135b53071
daizihan233 <y066ydg5@duck.com>
2023-08-28 20:27:40 +0800

    改了挺多的，我也不知道怎么写这个Commit的主题（

--- botfunc.py
+++ botfunc.py
@@ -61,10 +61,7 @@ except FileNotFoundError:
 try:
     dyn_yaml = yaml.safe_load(open('dynamic_config.yaml', 'r', encoding='UTF-8'))
 except FileNotFoundError:
-    safe_file_write('dynamic_config.yaml', """mute:
-- null
-word:
-- null""")
+    safe_file_write('dynamic_config.yaml', "")
     logger.warning('dynamic_config.yaml 已被程序自动创建')
 
 
