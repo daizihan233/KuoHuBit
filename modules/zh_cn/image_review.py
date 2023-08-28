99a146924b71e170bf0a86a3e3a7fc584dd78ec7
daizihan233 <y066ydg5@duck.com>
2023-08-28 20:49:54 +0800

    debug

--- modules/zh_cn/image_review.py
+++ modules/zh_cn/image_review.py
@@ -119,6 +119,7 @@ async def stop_word(app: Ariadne, group: Group, event: GroupMessage):
     )
 )
 async def image_review(app: Ariadne, message: MessageChain, event: GroupMessage):
+    logger.debug(botfunc.get_dyn_config("img"))
     if event.sender.id in botfunc.get_dyn_config("img"):
         for i in message[Image]:
             result = await using_tencent_cloud(base64.b64encode(i.get_bytes()).decode(), event.sender.id)
