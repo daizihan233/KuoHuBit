e987569eadd2710186ba127d2d19e36260938a63
daizihan233 <y066ydg5@duck.com>
2024-02-16 16:17:56 +0800

    fix

--- modules/gpt.py
+++ modules/gpt.py
@@ -237,10 +237,10 @@ async def accept(app: Ariadne, message: MessageChain = DetectPrefix("accept ")):
 @channel.use(
     ListenerSchema(
         listening_events=[FriendMessage],
-        decorators=[DetectPrefix("accept "), depen.check_friend_su()],
+        decorators=[DetectPrefix("deny "), depen.check_friend_su()],
     )
 )
-async def accept(app: Ariadne, message: MessageChain = DetectPrefix("accept ")):
+async def deny(app: Ariadne, message: MessageChain = DetectPrefix("accept ")):
     cache_var.cue_status[int(str(message))] = True
     await botfunc.run_sql("""UPDATE cue SET status=true WHERE ids=%s""", int(str(message)))
     if int(str(message)) == cache_var.cue_who[int(str(message))]:  # 是私聊
