e3d32143a78a0121bf0069b3abc9b5ef172b16b4
daizihan233 <y066ydg5@duck.com>
2024-02-18 18:37:58 +0800

    fix

--- modules/gpt.py
+++ modules/gpt.py
@@ -341,7 +341,7 @@ async def accept(app: Ariadne, message: MessageChain = DetectPrefix("accept ")):
 )
 async def deny(app: Ariadne, message: MessageChain = DetectPrefix("deny ")):
     cache_var.cue_status[int(str(message))] = False
-    await botfunc.run_sql("""UPDATE cue SET status=true WHERE ids=%s""", int(str(message)))
+    await botfunc.run_sql("""DELETE FROM cue WHERE ids=%s""", int(str(message)))
     if int(str(message)) == cache_var.cue_who[int(str(message))]:  # 是私聊
         await app.send_friend_message(
             target=int(str(message)),
