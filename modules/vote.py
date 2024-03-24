ded4ca2ca7147f487e982afb2ced420d44566938
daizihan233 <y066ydg5@duck.com>
2024-03-24 18:26:50 +0800

    新bug（

--- modules/vote.py
+++ modules/vote.py
@@ -175,12 +175,12 @@ async def initiate_single(
     opt_str = "\n".join([f"{x[0]}.{x[1]}" for x in enumerate(options["options"].keys(), 1)])
     send_msg = (
         f"⟨{results[0]}⟩ 号表决已创建！内容如下：\n"
-        f"类型：单选投票\n"
+        f"类型：多选投票\n"
         f"发起者：{member.id}\n"
         f"仅可参加：{options['accept']}\n"
         f"不可参加：{options['deny']}\n"
         f"最多可选：{'全部' if options['max'] == -1 else options['max']} 项\n"
-        f"最少需选：{options['min']}\n"
+        f"最少需选：{options['min']} 项\n"
         f"标题：{main_args['title']}\n"
         f"选项：\n"
         f"{opt_str}"
@@ -208,7 +208,7 @@ async def initiate_single(
     }
 
     await botfunc.run_sql(
-        """INSERT INTO vote (gid, uid, type, status, result, title, options) VALUES (%s, %s, 1, false, -1, %s, %s);""",
+        """INSERT INTO vote (gid, uid, type, status, result, title, options) VALUES (%s, %s, 2, false, -1, %s, %s);""",
         (group.id, member.id, main_args["title"], json.dumps(options, ensure_ascii=False)),
     )
     results = await botfunc.select_fetchone(
@@ -218,7 +218,7 @@ async def initiate_single(
     opt_str = "\n".join([f"{x[0]}.{x[1]}" for x in enumerate(options["options"].keys(), 1)])
     send_msg = (
         f"⟨{results[0]}⟩ 号表决已创建！内容如下：\n"
-        f"类型：单选投票\n"
+        f"类型：比重投票\n"
         f"发起者：{member.id}\n"
         f"仅可参加：{options['accept']}\n"
         f"不可参加：{options['deny']}\n"
