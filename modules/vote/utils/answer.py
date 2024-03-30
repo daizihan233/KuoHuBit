6884246e458e5d129f57aacc951d92d095c5745f
daizihan233 <y066ydg5@duck.com>
2024-03-30 23:52:02 +0800

    把投票部分的框架写了

--- modules/vote/utils/answer.py
+++ modules/vote/utils/answer.py
@@ -1,4 +1,84 @@
 #  本项目遵守 AGPL-3.0 协议，项目地址：https://github.com/daizihan233/MiraiHanBot
 
-class Answer:
-    ...
+from arclet.alconna import Args, Arg, CommandMeta, MultiVar, Alconna
+
+vid = Args(Arg("id#投票ID", int))
+one_option = Args(Arg("option#选项序号", int))
+multiple_option = Args(Arg("options#选项序号", MultiVar(int)))
+single = Alconna(
+    "单选投票", vid, one_option,
+    meta=CommandMeta(
+        "向一个单项选择投票进行投票",
+        usage="传入投票ID（即第多少号表决）和一个选项即可，顺序不可颠倒，对投票有异议可选择 -1，用空格隔开",
+        example="单选投票 1 3",
+    )
+)
+
+multiple = Alconna(
+    "多选投票", vid, multiple_option,
+    meta=CommandMeta(
+        "向一个多项选择投票进行投票",
+        usage="传入投票ID（即第多少号表决）和一个或多个选项即可，顺序不可颠倒，对投票有异议可选择 -1，用空格隔开",
+        example="多选投票 1 3 4 7 8",
+    ),
+    separators="\n"
+)
+
+proportion = Alconna(
+    "比重投票", vid, multiple_option,
+    meta=CommandMeta(
+        "对一个比重投票进行投票",
+        usage="传入投票ID（即第多少号表决）并按照顺序传入每个选项的比重即可，顺序不可颠倒，对投票有异议可仅传入投票序号与 -1，用空格隔开",
+        example="比重投票 3 12 5 4 9",
+    ),
+    separators="\n"
+)
+
+score = Alconna(
+    "评分投票", vid, multiple_option,
+    meta=CommandMeta(
+        "对一个评分投票机进行投票",
+        usage="传入投票ID（即第多少号表决）并按照顺序传入每个选项的评分即可，顺序不可颠倒，对投票有异议可仅传入投票序号与 -1，用空格隔开",
+        example="评分投票 1 5 5 3 2 1",
+    ),
+    separators="\n"
+)
+
+sort = Alconna(
+    "排序投票", vid, multiple_option,
+    meta=CommandMeta(
+        "对一个排序投票进行投票",
+        usage="传入投票ID（即第多少号表决）并按照顺序传入每个选项的排序后对应的序号即可，顺序不可颠倒，对投票有异议可仅传入投票序号与 -1，用空格隔开",
+        example="排序投票 1 4 5 3 2 1"
+    ),
+    separators="\n"
+)
+
+# 特殊的三个
+stop = Alconna(
+    "结束投票", vid,
+    meta=CommandMeta(
+        "结束一个正在进行的投票",
+        usage="传入投票ID（即第多少号表决）即可，然后将立刻进行统计，结束后无法再次开启",
+        example="结束投票 1"
+    ),
+    separators="\n"
+)
+delete = Alconna(
+    "删除投票", vid,
+    meta=CommandMeta(
+        "删除自己的投出的票",
+        usage="传入投票ID（即第多少号表决）即可，删除后无法恢复数据",
+        example="结束投票 1"
+    ),
+    separators="\n"
+)
+close = Alconna(
+    "废弃投票", vid,
+    meta=CommandMeta(
+        "废弃一个投票",
+        usage="传入投票ID（即第多少号表决）即可，然后将立刻进行统计，废弃后无法再次开启",
+        example="废弃投票 1"
+    ),
+    separators="\n"
+)
--- modules/vote/vote.py
+++ modules/vote/vote.py
@@ -12,7 +12,7 @@ from graia.saya.channel import ChannelMeta
 from loguru import logger
 
 import botfunc
-from modules.vote.utils.problem import Problem
+from .utils import problem, answer
 
 channel = Channel[ChannelMeta].current()
 channel.meta["name"] = "投票"
@@ -22,7 +22,7 @@ channel.meta["author"] = "KuoHu"
 GET_MAX_ID = "SELECT MAX(ids) FROM vote"
 
 
-@channel.use(AlconnaSchema(AlconnaDispatcher(Problem.single, skip_for_unmatch=False)))
+@channel.use(AlconnaSchema(AlconnaDispatcher(problem.single, skip_for_unmatch=False)))
 @channel.use(ListenerSchema(listening_events=[GroupMessage]))
 async def initiate_single(
         app: Ariadne,
@@ -60,7 +60,7 @@ async def initiate_single(
     await app.send_group_message(group, send_msg)
 
 
-@channel.use(AlconnaSchema(AlconnaDispatcher(Problem.multiple, skip_for_unmatch=False)))
+@channel.use(AlconnaSchema(AlconnaDispatcher(problem.multiple, skip_for_unmatch=False)))
 @channel.use(ListenerSchema(listening_events=[GroupMessage]))
 async def initiate_single(
         app: Ariadne,
@@ -103,7 +103,7 @@ async def initiate_single(
     await app.send_group_message(group, send_msg)
 
 
-@channel.use(AlconnaSchema(AlconnaDispatcher(Problem.proportion, skip_for_unmatch=False)))
+@channel.use(AlconnaSchema(AlconnaDispatcher(problem.proportion, skip_for_unmatch=False)))
 @channel.use(ListenerSchema(listening_events=[GroupMessage]))
 async def initiate_single(
         app: Ariadne,
@@ -118,8 +118,7 @@ async def initiate_single(
         "options": {key: 0 for key in main_args["option"]},
         "deny": opt_args.get("deny", []),
         "accept": opt_args.get("accept", []),
-        "max": main_args["max"],
-        "sort": opt_args.get("sort", False)
+        "max": main_args["max"]
     }
 
     await botfunc.run_sql(
@@ -144,3 +143,165 @@ async def initiate_single(
         f"{opt_str}"
     )
     await app.send_group_message(group, send_msg)
+
+
+@channel.use(AlconnaSchema(AlconnaDispatcher(problem.score, skip_for_unmatch=False)))
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def initiate_score(
+        app: Ariadne,
+        group: Group,
+        member: Member,
+        result: Arparma
+):
+    logger.debug(result)
+    main_args = result.main_args
+    opt_args = result.other_args
+    options = {
+        "options": {key: 0 for key in main_args["option"]},
+        "deny": opt_args.get("deny", []),
+        "accept": opt_args.get("accept", []),
+        "max": opt_args.get("max", 5),
+        "min": opt_args.get("min", 1),
+        "int": opt_args.get("int", True),
+    }
+
+    await botfunc.run_sql(
+        """INSERT INTO vote (gid, uid, type, status, result, title, options) VALUES (%s, %s, 3, false, -1, %s, %s);""",
+        (group.id, member.id, main_args["title"], json.dumps(options, ensure_ascii=False)),
+    )
+    results = await botfunc.select_fetchone(
+        GET_MAX_ID
+    )
+
+    opt_str = "\n".join([f"{x[0]}.{x[1]}" for x in enumerate(options["options"].keys(), 1)])
+    send_msg = (
+        f"⟨{results[0]}⟩ 号表决已创建！内容如下：\n"
+        f"类型：评分投票\n"
+        f"发起者：{member.id}\n"
+        f"仅可参加：{options['accept']}\n"
+        f"不可参加：{options['deny']}\n"
+        f"可选最高分：{options['max']}\n"
+        f"可选最低分：{options['min']}\n"
+        f"必须为整数：{options['int']}\n"
+        f"标题：{main_args['title']}\n"
+        f"选项：\n"
+        f"{opt_str}"
+    )
+    await app.send_group_message(group, send_msg)
+
+
+@channel.use(AlconnaSchema(AlconnaDispatcher(problem.sort, skip_for_unmatch=False)))
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def initiate_sort(
+        app: Ariadne,
+        group: Group,
+        member: Member,
+        result: Arparma
+):
+    logger.debug(result)
+    main_args = result.main_args
+    opt_args = result.other_args
+    options = {
+        "options": {key: 0 for key in main_args["option"]},
+        "deny": opt_args.get("deny", []),
+        "accept": opt_args.get("accept", [])
+    }
+
+    await botfunc.run_sql(
+        """INSERT INTO vote (gid, uid, type, status, result, title, options) VALUES (%s, %s, 4, false, -1, %s, %s);""",
+        (group.id, member.id, main_args["title"], json.dumps(options, ensure_ascii=False)),
+    )
+    results = await botfunc.select_fetchone(
+        GET_MAX_ID
+    )
+
+    opt_str = "\n".join([f"{x[0]}.{x[1]}" for x in enumerate(options["options"].keys(), 1)])
+    send_msg = (
+        f"⟨{results[0]}⟩ 号表决已创建！内容如下：\n"
+        f"类型：排序投票\n"
+        f"发起者：{member.id}\n"
+        f"仅可参加：{options['accept']}\n"
+        f"不可参加：{options['deny']}\n"
+        f"标题：{main_args['title']}\n"
+        f"选项：\n"
+        f"{opt_str}"
+    )
+    await app.send_group_message(group, send_msg)
+
+
+@channel.use(AlconnaSchema(AlconnaDispatcher(answer.single, skip_for_unmatch=False)))
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def initiate_sort(
+        app: Ariadne,
+        group: Group,
+        member: Member,
+        result: Arparma
+):
+    logger.debug(result)
+    main_args = result.main_args
+    ...  # TODO
+
+
+@channel.use(AlconnaSchema(AlconnaDispatcher(answer.single, skip_for_unmatch=False)))
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def initiate_sort(
+        app: Ariadne,
+        group: Group,
+        member: Member,
+        result: Arparma
+):
+    logger.debug(result)
+    main_args = result.main_args
+    ...  # TODO
+
+
+@channel.use(AlconnaSchema(AlconnaDispatcher(answer.multiple, skip_for_unmatch=False)))
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def initiate_sort(
+        app: Ariadne,
+        group: Group,
+        member: Member,
+        result: Arparma
+):
+    logger.debug(result)
+    main_args = result.main_args
+    ...  # TODO
+
+
+@channel.use(AlconnaSchema(AlconnaDispatcher(answer.proportion, skip_for_unmatch=False)))
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def initiate_sort(
+        app: Ariadne,
+        group: Group,
+        member: Member,
+        result: Arparma
+):
+    logger.debug(result)
+    main_args = result.main_args
+    ...  # TODO
+
+
+@channel.use(AlconnaSchema(AlconnaDispatcher(answer.score, skip_for_unmatch=False)))
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def initiate_sort(
+        app: Ariadne,
+        group: Group,
+        member: Member,
+        result: Arparma
+):
+    logger.debug(result)
+    main_args = result.main_args
+    ...  # TODO
+
+
+@channel.use(AlconnaSchema(AlconnaDispatcher(answer.sort, skip_for_unmatch=False)))
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def initiate_sort(
+        app: Ariadne,
+        group: Group,
+        member: Member,
+        result: Arparma
+):
+    logger.debug(result)
+    main_args = result.main_args
+    ...  # TODO
